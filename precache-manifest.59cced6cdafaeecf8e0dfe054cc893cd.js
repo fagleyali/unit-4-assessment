16b54b8bc6abfdca8c6dd0b57eaabe9131c02343
Student Workspace <student@Candices-MacBook-Air.local>
2019-04-15 17:18:42 -0500

    Updates

---
+++ precache-manifest.59cced6cdafaeecf8e0dfe054cc893cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec951fbe533f09d32db6",
+    "url": "/fagleyali/unit-4-assessment/static/js/runtime~main.5c6c3940.js"
+  },
+  {
+    "revision": "daa9ad7589da344ebf51",
+    "url": "/fagleyali/unit-4-assessment/static/js/main.533ef456.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fagleyali/unit-4-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "daa9ad7589da344ebf51",
+    "url": "/fagleyali/unit-4-assessment/static/css/main.21866834.chunk.css"
+  },
+  {
+    "revision": "948e0ab5ebb7da9880cef8c8cc7a2d69",
+    "url": "/fagleyali/unit-4-assessment/index.html"
+  }
+];
